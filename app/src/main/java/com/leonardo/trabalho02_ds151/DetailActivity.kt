e855f42683f9ba8fef31fa23b3b3e78022d28509
Leonardo Eugênio Panceri de Araujo <panceri.leo@gmail.com>
2024-11-01 19:11:45 -0300

    first commit

---
+++ app/src/main/java/com/leonardo/trabalho02_ds151/DetailActivity.kt
@@ -0,0 +1,38 @@
+package com.leonardo.trabalho02_ds151
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val closeBtn = findViewById<Button>(R.id.closeBtn)
+        val bundle = intent.extras
+        if(bundle != null){
+            val user = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                bundle.getParcelable("user", User::class.java)
+            }else{
+                bundle.getParcelable("user")
+            }
+            println(user?.userName)
+            println(user?.n1Peso)
+            println(user?.n1)
+        }
+        closeBtn.setOnClickListener(){
+            finish()
+        }
+    }
+}
